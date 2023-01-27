7074d9a34d48275f011d5f530846341576af58f5
Nitsua28 <achan113@ucmerced.edu>
2023-01-27 00:28:43 -0800

    working slide Calculator

---
+++ script.js
@@ -0,0 +1,89 @@
+let input = ""
+let history = ""
+let operator = ""
+
+function rerender() {
+    const slider = document.getElementById("myRange");
+    const slideValue = document.getElementById("slideValue");
+    slideValue.innerHTML = slider.value;
+
+    const outputValue = document.getElementById("outputValue");
+    outputValue.innerHTML = input;
+
+    const historyValue = document.getElementById("historyValue");
+    historyValue.innerHTML = history;
+}
+
+rerender()
+
+function input_button(){
+    const slider = document.getElementById("myRange");
+    input = input + slider.value;
+    rerender();
+}
+
+function resetOutput() {
+    history = input;
+    input = ""
+    rerender()
+}
+
+function clear_button() {
+    console.log("cleared")
+    history = "";
+    input = "";
+    operator = "";
+    rerender()
+}
+function operate(symbol){
+    if ((history != "") && (input != "")) calculate();
+    
+    switch(symbol){
+        case "+":
+            operator = "+";
+            break
+        case "-":
+            operator = "-";
+            break
+        case "*":
+            operator = "*";
+            break
+        case "/":
+            operator = "/";
+            break
+        default:
+            break
+        
+    }
+    resetOutput()
+}
+function calculate(){
+    
+    if (operator != "" && input != "") {
+        switch (operator){
+            case "+":
+                input = (parseInt(history) + parseInt(input)).toString()
+                break
+            case "-":
+                input = (parseInt(history) - parseInt(input)).toString()
+                break
+            case "*":
+                input = (parseInt(history) * parseInt(input)).toString()
+                break
+            case "/":
+                input = (parseInt(history) / parseInt(input)).toString()
+            default:
+                console.log("error");
+        }
+    }
+    history = "";
+    rerender()
+}
+
+
+
+
+
+
+
+
