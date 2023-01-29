9f485f31652bf22717c68176db674e6cc6cc135c
Nitsua28 <achan113@ucmerced.edu>
2023-01-28 16:50:27 -0800

    final edits

--- script.js
+++ script.js
@@ -1,89 +1,80 @@
-let input = ""
-let history = ""
-let operator = ""
+let input = ''
+let history = ''
+let operator = ''
 
-function rerender() {
-    const slider = document.getElementById("myRange");
-    const slideValue = document.getElementById("slideValue");
-    slideValue.innerHTML = slider.value;
+function rerender () {
+  const slider = document.getElementById('myRange')
+  const slideValue = document.getElementById('slideValue')
+  slideValue.innerHTML = slider.value
 
-    const outputValue = document.getElementById("outputValue");
-    outputValue.innerHTML = input;
+  const outputValue = document.getElementById('outputValue')
+  outputValue.innerHTML = input
 
-    const historyValue = document.getElementById("historyValue");
-    historyValue.innerHTML = history;
+  const historyValue = document.getElementById('historyValue')
+  historyValue.innerHTML = history
 }
 
 rerender()
 
-function input_button(){
-    const slider = document.getElementById("myRange");
-    input = input + slider.value;
-    rerender();
+function input_button () {
+  const slider = document.getElementById('myRange')
+  input = input + slider.value
+  rerender()
 }
 
-function resetOutput() {
-    history = input;
-    input = ""
-    rerender()
+function resetOutput () {
+  history = input
+  input = ''
+  rerender()
 }
 
-function clear_button() {
-    console.log("cleared")
-    history = "";
-    input = "";
-    operator = "";
-    rerender()
+function clear_button () {
+  console.log('cleared')
+  history = ''
+  input = ''
+  operator = ''
+  rerender()
 }
-function operate(symbol){
-    if ((history != "") && (input != "")) calculate();
-    
-    switch(symbol){
-        case "+":
-            operator = "+";
-            break
-        case "-":
-            operator = "-";
-            break
-        case "*":
-            operator = "*";
-            break
-        case "/":
-            operator = "/";
-            break
-        default:
-            break
-        
+function operate (symbol) {
+  if ((history !== '') && (input !== '')) calculate()
+
+  switch (symbol) {
+    case '+':
+      operator = '+'
+      break
+    case '-':
+      operator = '-'
+      break
+    case '*':
+      operator = '*'
+      break
+    case '/':
+      operator = '/'
+      break
+    default:
+      break
+  }
+  resetOutput()
+}
+function calculate () {
+  if (operator !== '' && input !== '') {
+    switch (operator) {
+      case '+':
+        input = (parseInt(history) + parseInt(input)).toString()
+        break
+      case '-':
+        input = (parseInt(history) - parseInt(input)).toString()
+        break
+      case '*':
+        input = (parseInt(history) * parseInt(input)).toString()
+        break
+      case '/':
+        input = (parseInt(history) / parseInt(input)).toString()
+        break
+      default:
+        console.log('error')
     }
-    resetOutput()
+  }
+  history = ''
+  rerender()
 }
-function calculate(){
-    
-    if (operator != "" && input != "") {
-        switch (operator){
-            case "+":
-                input = (parseInt(history) + parseInt(input)).toString()
-                break
-            case "-":
-                input = (parseInt(history) - parseInt(input)).toString()
-                break
-            case "*":
-                input = (parseInt(history) * parseInt(input)).toString()
-                break
-            case "/":
-                input = (parseInt(history) / parseInt(input)).toString()
-            default:
-                console.log("error");
-        }
-    }
-    history = "";
-    rerender()
-}
-
-
-
-
-
-
-
-
